15fe00cf608d932a82879de6c91fae8e2462810d
Rayer Tung <rayer.tung@appier.com>
2022-02-15 23:23:38 +0800

    Complete feature

--- Fee.go
+++ Fee.go
@@ -26,6 +26,6 @@ func GetLimitString(limit int) string {
 	return fmt.Sprintf("%d GB", limit)
 }
 
-//func (f FeeEntry) String() string {
-//	return fmt.Sprintf("%v[%v](%v)\t相當月繳 %v\t實際月繳 %v\t用量限制 %v", f.Provider, f.Plan, f.SignalType, f.RealMonthlyCost(), f.MonthlyFee, GetLimitString(f.Limit))
-//}
+func (f FeeEntry) String() string {
+	return fmt.Sprintf("%v[%v](%v)\t合約期 %v\t每月單價 %v\t實際月繳 %v\t用量限制 %v\t%v", f.Provider, f.Plan, f.SignalType, f.ContractMonth, f.RealMonthlyCost(), f.MonthlyFee, GetLimitString(f.Limit), f.Description)
+}
--- Parser.go
+++ Parser.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
@@ -19,18 +20,9 @@ const (
 )
 
 func ParseLandmark() (ret []FeeEntry) {
-	reader := strings.NewReader(target)
-	doc, err := goquery.NewDocumentFromReader(reader)
-	if err != nil {
-		panic(err)
-	}
-	var providers []string
-	doc.Find(".brand-tags span").Each(func(i int, selection *goquery.Selection) {
-		providers = append(providers, selection.Text())
-	})
-
-	//order of providers will be used in next parse
-	//id will be "tab-index-{order}"
+	client := http.Client{}
+	providerQueryArg := []string{"cht", "fet", "twn", "tstar", "apgt"}
+	providerTwNames := []string{"中華電信", "遠傳", "台灣大哥大", "台灣之星", "亞太電信"}
 
 	planTypes := []string{
 		np, renew, newUser,
@@ -47,71 +39,77 @@ func ParseLandmark() (ret []FeeEntry) {
 	feeMonthRegex := regexp.MustCompile(`(\d+)+\((\d+)\)`)
 	limitRegex := regexp.MustCompile(`(\d+)+`)
 
-	for i, provider := range providers {
-		providerNode := doc.Find(fmt.Sprintf("#tab-index-%d", i))
-		for planTypeIndex, _ := range planTypes {
-			providerNode.Find("div .tab-content.no-border.no-shadow").ChildrenFiltered("div").Each(func(i int, priceTable *goquery.Selection) {
-				signalType := signalTypes[0]
-				priceTable.Find("tr").Each(func(i int, priceRow *goquery.Selection) {
-					priceDetail := priceRow.Find("td")
-					if len(priceDetail.Nodes) == 0 {
-						return
-					}
-					//Handle entries
-					index := 0
-					feeAndMonth := feeMonthRegex.FindStringSubmatch(priceDetail.Eq(index).Text())
-					index++
-					fee, _ := strconv.Atoi(feeAndMonth[1])
-					months, _ := strconv.Atoi(feeAndMonth[2])
-					rebate, _ := strconv.Atoi(strings.Join(strings.Fields(priceDetail.Eq(index).Text()), ""))
-					index++
-					prepaid, _ := strconv.Atoi(strings.Join(strings.Fields(priceDetail.Eq(index).Text()), ""))
-					index++
-
-					//處理傳輸量
-					limit := 0
-					limitText := priceDetail.Eq(index).Text()
-					index++
-					limitParse := limitRegex.FindStringSubmatch(limitText)
-					if len(limitParse) != 0 {
-						limit, _ = strconv.Atoi(limitParse[1])
-					}
-
-					//處理hotspot傳輸量(5g only)
-					hotspotLimit := 0
-					if signalType == signalTypes[1] {
-						hotspotLimitText := priceDetail.Eq(index).Text()
-						index++
-						hotspotLimitParse := limitRegex.FindStringSubmatch(hotspotLimitText)
-						if len(hotspotLimitParse) != 0 {
-							hotspotLimit, _ = strconv.Atoi(hotspotLimitParse[1])
+	for pi, providerArg := range providerQueryArg {
+		providerTwName := providerTwNames[pi]
+		resp, _ := client.Get("https://www.landtop.com.tw/discount?provider=" + providerArg)
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		providerNode := doc.Find(fmt.Sprintf("#tab-index-0"))
+		for planTypeIndex, planType := range planTypes {
+			providerNode.Find("div#" + planType).Each(func(planOrder int, planTable *goquery.Selection) {
+				planTable.Find("table").Each(func(signalTypeOrder int, priceTable *goquery.Selection) {
+					priceTable.Find("tr").Each(func(i int, priceRow *goquery.Selection) {
+						signalType := signalTypes[signalTypeOrder]
+						priceDetail := priceRow.Find("td")
+						if len(priceDetail.Nodes) == 0 {
+							return
 						}
-					}
+						//Handle entries
+						index := 0
+						feeAndMonth := feeMonthRegex.FindStringSubmatch(priceDetail.Eq(index).Text())
+						index++
+						fee, _ := strconv.Atoi(feeAndMonth[1])
+						months, _ := strconv.Atoi(feeAndMonth[2])
+						rebate, _ := strconv.Atoi(strings.Join(strings.Fields(priceDetail.Eq(index).Text()), ""))
+						index++
+						prepaid, _ := strconv.Atoi(strings.Join(strings.Fields(priceDetail.Eq(index).Text()), ""))
+						index++
 
-					description := priceDetail.Eq(index).Text()
+						//處理傳輸量
+						limit := 0
+						limitText := priceDetail.Eq(index).Text()
+						index++
+						limitParse := limitRegex.FindStringSubmatch(limitText)
+						if len(limitParse) != 0 {
+							limit, _ = strconv.Atoi(limitParse[1])
+						}
 
-					ret = append(ret, FeeEntry{
-						Provider:      provider,
-						SignalType:    signalType,
-						Limit:         limit,
-						HotspotLimit:  hotspotLimit,
-						ContractMonth: months,
-						PrePaid:       prepaid,
-						MonthlyFee:    fee,
-						Rebate:        rebate,
-						Plan:          planText[planTypeIndex],
-						Description:   description,
+						//處理hotspot傳輸量(5g only)
+						hotspotLimit := 0
+						if signalType == signalTypes[1] {
+							hotspotLimitText := priceDetail.Eq(index).Text()
+							index++
+							hotspotLimitParse := limitRegex.FindStringSubmatch(hotspotLimitText)
+							if len(hotspotLimitParse) != 0 {
+								hotspotLimit, _ = strconv.Atoi(hotspotLimitParse[1])
+							}
+						}
+
+						description := priceDetail.Eq(index).Text()
+
+						ret = append(ret, FeeEntry{
+							Provider:      providerTwName,
+							SignalType:    signalType,
+							Limit:         limit,
+							HotspotLimit:  hotspotLimit,
+							ContractMonth: months,
+							PrePaid:       prepaid,
+							MonthlyFee:    fee,
+							Rebate:        rebate,
+							Plan:          planText[planTypeIndex],
+							Description:   description,
+						})
 					})
 				})
-
 			})
-
 		}
-
+		_ = resp.Body.Close()
 	}
 
 	for _, r := range ret {
-		if r.Limit == 0 || r.Limit > 36 {
+		if (r.Limit == 0 || r.Limit > 40) && r.RealMonthlyCost() < 700 && r.Plan == "NP" {
 			fmt.Println(r)
 		}
 	}
